a12299bc3e673d15dbb87b3bde53a5511bf25adc
unknown <ricardo.fer.oliveira964@gmail.com>
2022-06-08 23:29:15 -0400

    update properties

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+	
 	@GetMapping
 	public String getBook() {
 		return "API Test";
